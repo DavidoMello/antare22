ef33b620ad5571f299d4175e6e285e054116c19f
David Mello <138627500+DavidoMello@users.noreply.github.com>
2023-09-11 11:58:58 -0300

    Add files via upload

--- js/cliente.js
+++ js/cliente.js
@@ -1,36 +1,31 @@
-const enderecoForm = document .querySelector(".form-body");
-const cepInput = document.querySelector("#cep");
-const ruaInput = document.querySelector("#rua");
-const bairroInput = document.querySelector("#bairro");
-const cidadeInput = document.querySelector("#cidade");
-const estadoInput = document.querySelector("#estado");
+var enderecoForm = document .querySelector(".form-body");
+var cepInput = document.querySelector("#cep");
+var ruaInput = document.querySelector("#rua");
+var bairroInput = document.querySelector("#bairro");
+var cidadeInput = document.querySelector("#cidade");
+var estadoInput = document.querySelector("#estado");
+var formInput = document.querySelector("data-input")
 
-capInput.addEventListener("keypress",(e) => {
 
 
-    const onlyNumbers = /[0-9]/;
-    const tecla = String.fromCharCode(e.keyCode);
-    
-    if(!onlyNumbers.test(tecla)){
-        e.preventDefault();
-        return;
-    }
-
-});
-
-cepInput.addEventListener("keyup", (e) =>){
+cepInput.addEventListener("keyup", (e) =>{
 
     const inputValue = e.target.value;
-    if(inputValue.length ===8){
-        //fazer alguma coisa - rodar a apalicação
+    if(inputValue.length === 8){
+        getEndereco(inputValue); //fazer alguma coisa - rodar a apalicação
     }
 
 });
 
-const getEndereco =(cep) => {
+var getEndereco = async (cep) => {
     const apiURL = `https://viacep.com.br/ws/${cep}/json/`;
     const response = await fetch(apiURL);
     const data = await response.json();
     console.log(data);
-}
+    console.log(formInput);
+    console.log(data.erro);
+    ruaInput.value = data.localidade;
+    bairroInput.value = data.uf;
+};
+
 
